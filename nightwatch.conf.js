e89c1c7f2aa880823522e00d08e6328e17446e0e
Davor Eric <davor.eric@trikoder.com>
2016-07-15 10:11:11 +0200

    modified nightwatch integration and minor version bump

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,4 +1,49 @@
 module.exports = (function(settings) {
-  settings.test_workers = false;
-  return settings;
-})(require('./nightwatch.json'));
+
+    settings.test_workers = false;
+    return settings;
+
+})({
+
+    "src_folders" : ["tests"],
+    "output_folder" : "reports",
+
+    "selenium" : {
+
+        "start_process" : false,
+        "host" : "hub.browserstack.com",
+        "port" : 80
+
+    },
+
+    "test_settings" : {
+
+        "default" : {
+
+            "launch_url" : "http://hub.browserstack.com",
+            "selenium_port"  : 80,
+            "selenium_host"  : "hub.browserstack.com",
+            "silent": true,
+            "screenshots" : {
+                "enabled" : false,
+                "path" : ""
+            },
+
+            "desiredCapabilities": {
+
+                "browserName": "Chrome",
+                "browser_version": "46.0",
+                "os": "Windows",
+                "os_version": "8.1",
+                "javascriptEnabled": true,
+                "acceptSslCerts": true,
+                "browserstack.user": process.env.BROWSER_STACK_USERNAME,
+                "browserstack.key": process.env.BROWSER_STACK_ACCESS_KEY
+
+            }
+
+        }
+
+    }
+
+});
